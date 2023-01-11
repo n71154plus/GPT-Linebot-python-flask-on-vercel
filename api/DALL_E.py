a161fc44387aa745f267b1b969a1784d3cf487f2
n71154plus <86144527+n71154plus@users.noreply.github.com>
2023-01-11 09:50:50 +0000

    Create DALL_E.py

---
+++ api/DALL_E.py
@@ -0,0 +1,27 @@
+from api.prompt import Prompt
+
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class DALL_E:
+    def __init__(self):
+        self.prompt = Prompt()
+        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+
+    def get_response(self):
+        response = openai.Image.create(
+            prompt=self.prompt.generate_prompt(),
+            n=1,
+            size="256x256"
+        )
+        return response["data"][0]["url"]
+
+    def add_msg(self, text):
+        self.prompt.add_msg(text)
