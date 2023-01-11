0760f0d6cce035cc70c453f77bae266e6d1c943a
n71154plus <86144527+n71154plus@users.noreply.github.com>
2023-01-11 09:59:21 +0000

    Update DALL_E.py

--- api/DALL_E.py
+++ api/DALL_E.py
@@ -15,9 +15,9 @@ class DALL_E:
         self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
 
-    def get_response(self):
+    def get_response(self,text1):
         response = openai.Image.create(
-            prompt=self.prompt.generate_prompt(),
+            prompt=text1,
             n=1,
             size="256x256"
         )
