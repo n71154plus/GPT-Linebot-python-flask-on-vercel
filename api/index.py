72a2a361e7352f93f26517d9dfc3a4668ad6c1d8
n71154plus <86144527+n71154plus@users.noreply.github.com>
2023-02-07 10:55:51 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -60,7 +60,7 @@ def handle_message(event):
             event.reply_token, [
                 TextSendMessage(text='Display name: ' + profile.display_name),
                 TextSendMessage(text='Status message: ' + str(profile.status_message)),
-                TextSendMessage(text='UserID: ' + profile.userid)
+                TextSendMessage(text='UserID: ' + event.source.user_id)
             ]
         )
         return
